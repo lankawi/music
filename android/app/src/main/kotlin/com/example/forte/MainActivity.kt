643127a22be3c1788161940a09fe1867f69a3726
lankawi <linavincelovic@gmail.com>
2023-02-24 23:03:31 +0300

    load

---
+++ android/app/src/main/kotlin/com/example/forte/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.forte
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:forte/myapp_icons.dart';
+
+void main() {
+  runApp (
+    const MaterialApp (
+      home: Text (
+        "hi",
+        textDirection: TextDirection.ltr,
+      ),
+      color: Colors.yellow[100],
+    ),
+  );
+}
