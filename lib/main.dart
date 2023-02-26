97ad991cb0728f64adb64d4ff5da0f0a1f788d9b
lankawi <linavincelovic@gmail.com>
2023-02-26 23:38:06 +0300

    load

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,35 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:forte/myapp_icons.dart';
 
-void main() {
-  runApp (
-    const MaterialApp (
-      home: Text (
-        "hi",
-        textDirection: TextDirection.ltr,
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //запускает Firebase до запуска приложения
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        pageTransitionsTheme: const PageTransitionsTheme(builders: {
+          TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+        }),
       ),
-      color: Colors.yellow[100],
-    ),
-  );
+      routes: {
+        '/': (context) => const FirebaseStream(),
+        '/home': (context) => const HomeScreen(),
+        '/account': (context) => const AccountScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignUpScreen(),
+        '/reset_password': (context) => const ResetPasswordScreen(),
+        '/verify_email': (context) => const VerifyEmailScreen(),
+      },
+      initialRoute: '/',
+    );
+  }
 }
